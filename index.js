8bfe9663b35981b190cbef8f41bec1abc1aba11c
AdrianTerciado <terciadojano@gmail.com>
2024-04-17 12:01:40 +0200

    Funcionalidad barra de búsqueda

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const passport = require("passport");
+/* const jwt = require("jsonwebtoken");
+const passport = require("passport"); */
 
 //Inicializamos server
 const app = express();
