9168bfed99d1b30e8723a04feb1e7b7410e4549a
Vegas-in <angeldevegas@gmail.com>
2024-04-18 17:19:24 +0200

    despligue para render

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: `${process.env.CLIENT_ID}`,
     clientSecret: `${process.env.CLIENT_SECRET}`,
-    callbackURL: `http://localhost:3000/google/callBack`,
+    callbackURL: `${process.env.BASE_URL}/google/callBack`,
     proxy: true
   },
   function(request, accessToken, refreshToken, profile, done) {
