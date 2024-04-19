1421212e707f460bf02eeb2ba848739c3b3dca3e
Veronica Parra <vero.parragraca@gmail.com>
2024-04-19 09:31:55 +0200

    Estilos para resultados de busqueda

--- public/script.js
+++ public/script.js
@@ -1,17 +1,24 @@
 document.querySelector("#search_button").addEventListener("click", function () {
-
-    let looking_for = document.getElementById("text_search").value;
-    document.getElementById("cards-search-results").innerHTML = "";
-     fetch(`/api/search?search=${looking_for}`)
-        .then(res => res.json())
-        .then(data => data.forEach(element => 
-            document.getElementById("cards-search-results").innerHTML +=               
-                `<article class="cards_offers">
-                    <h3>Título: ${element.tituloOferta}</h3>
+  let looking_for = document.getElementById("text_search").value;
+  document.getElementById("cards-search-results").innerHTML = "";
+  fetch(`/api/search?search=${looking_for}`)
+    .then((res) => res.json())
+    .then((data) =>
+      data.forEach(
+        (element) =>
+          (document.getElementById(
+            "cards-search-results"
+          ).innerHTML += `<article class="card">
+                    <h2>Título: ${element.tituloOferta}</h2>
                     <p>Fecha: ${element.fechaOferta}</p>
                     <p>Salario ${element.salarioOferta}</p>
-                    <p>Descripción ${element.descripcionOferta}</p>
-                    <p>Link ${element.linkOferta}</p>
-                </article>` 
-         ))
+                    <h4>Descripción ${element.descripcionOferta}</h4>
+                    <label class="add-fav">
+                    <input type="checkbox" />
+                    <i class="fa-solid fa-heart"></i>
+                  </label>
+                    <a href="${element.linkOferta}">Link</a>
+                </article>`)
+      )
+    );
 });
